defd2898b7a2fc876ab59d8f47db60207d6065ba
9um4 <51898564+9um4@users.noreply.github.com>
2025-02-01 23:48:34 +0900

    Refactor: update ItemData class to use ID enum and enhance property accessors

--- Assets/Scripts/ItemData/ItemData.cs
+++ Assets/Scripts/ItemData/ItemData.cs
@@ -2,67 +2,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// 게임 내 아이템의 속성과 동작 로직을 저장하는 ScriptableObject
-/// </summary>
-[CreateAssetMenu(fileName = "UnnamedItemData", menuName="ItemData")]
-public class ItemData : ScriptableObject
+namespace ItemSystem
 {
     /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class의 기본 아이템 아이디
+    /// 게임 내 아이템의 속성과 동작 로직을 저장하는 ScriptableObject
     /// </summary>
-    [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class의 기본 아이템 아이디")]
-    private ItemID id = ItemID.AIR;
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class의 기본 이름
-    /// </summary>
-    [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class의 기본 이름")]
-    private string defaultName = "";
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class의 기본 설명
-    /// </summary>
-    [TextArea(3, 10)]
-    [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class의 기본 설명")]
-    private string defaultDescription = "";
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class가 한 스택 당 최대 쌓을 수 있는 아이템 개수
-    /// </summary>
-    [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class가 한 스택 당 최대 쌓을 수 있는 아이템 개수")]
-    private int defaultMaxStackAmount = 64;
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite 배열
-    /// </summary>
-    [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite 배열")]
-    private Sprite[] defaultSprites;
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite Index
-    /// </summary>
-    [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite Index")]
-    private int defaultSpriteIndex = 0;
+    [CreateAssetMenu(fileName = "UnnamedItemData", menuName = "ItemData")]
+    public class ItemData : ScriptableObject
+    {
+        #region ID
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class의 기본 아이템 아이디
+        /// </summary>
+        [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class의 기본 아이템 아이디")]
+        private ID id = ID.AIR;
 
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class의 기본 아이템 아이디
+        /// </summary>
+        public ID ID => id;
+        #endregion
 
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class의 기본 아이템 아이디
-    /// </summary>
-    public Material Material => material;
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class의 기본 이름
-    /// </summary>
-    public string DefaultName => defaultName;
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class의 기본 설명
-    /// </summary>
-    public string DefaultDescription => defaultDescription;
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class가 한 스택 당 최대 쌓을 수 있는 아이템 개수
-    /// </summary>
-    public int DefaultMaxStackAmount => defaultMaxStackAmount;
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite 배열
-    /// </summary>
-    public Sprite[] DefaultSprites => defaultSprites;
-    /// <summary>
-    /// 이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite Index
-    /// </summary>
-    public int DefaultSpriteIndex => defaultSpriteIndex;
+        #region name
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class의 기본 이름
+        /// </summary>
+        [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class의 기본 이름")]
+        private string defaultName = "";
+
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class의 기본 이름
+        /// </summary>
+        public string DefaultName => defaultName;
+        #endregion
+
+        #region description
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class의 기본 설명
+        /// </summary>
+        [TextArea(3, 10)]
+        [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class의 기본 설명")]
+        private string defaultDescription = "";
+
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class의 기본 설명
+        /// </summary>
+        public string DefaultDescription => defaultDescription;
+        #endregion
+
+        #region stack amount
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class가 한 스택 당 최대 쌓을 수 있는 아이템 개수
+        /// </summary>
+        [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class가 한 스택 당 최대 쌓을 수 있는 아이템 개수")]
+        private int defaultMaxStackAmount = 64;
+
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class가 한 스택 당 최대 쌓을 수 있는 아이템 개수
+        /// </summary>
+        public int DefaultMaxStackAmount => defaultMaxStackAmount;
+        #endregion
+
+        #region sprites
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite 배열
+        /// </summary>
+        [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite 배열")]
+        private Sprite[] defaultSprites;
+
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite 배열
+        /// </summary>
+        public Sprite[] DefaultSprites => defaultSprites;
+        #endregion
+
+        #region sprite index
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite Index
+        /// </summary>
+        [SerializeField, Tooltip("이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite Index")]
+        private int defaultSpriteIndex = 0;
+
+        /// <summary>
+        /// 이 ItemData를 갖는 Wrapper Class가 갖는 기본 Sprite Index
+        /// </summary>
+        public int DefaultSpriteIndex => defaultSpriteIndex;
+        #endregion
+    }
+
 }
+
