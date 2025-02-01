e22e4e33fd733f62132c55532e5fa025930ed224
9um4 <51898564+9um4@users.noreply.github.com>
2025-02-01 21:40:50 +0900

    Refactor: update Entity class structure and improve property accessors

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -1,87 +1,91 @@
 using UnityEngine;
 
-/// <summary>
-/// 게임 시스템 상 Entity로 분류되는 GameObject에 부착되는 컴포넌트의 기본 클래스
-/// </summary>
-public abstract class Entity: MonoBehaviour
+namespace EntitySystem
 {
-
-    #region entityData
     /// <summary>
-    /// Entity의 공통 로직을 담고 있는 ScriptableObject
+    /// 게임 시스템 상 Entity로 분류되는 GameObject에 부착되는 컴포넌트의 최상위 클래스
     /// </summary>
-    [SerializeField, Tooltip("Entity의 공통 로직을 담고 있는 ScriptableObject")]
-    protected EntityData entityData;
-
-    /// <summary>
-    /// Entity의 공통 로직을 담고 있는 ScriptableObject
-    /// </summary>
-    public EntityData EntityData => entityData;
-    #endregion
-
-    #region name
-    /// <summary>
-    /// Entity의 표시될 이름
-    /// </summary>
-    [SerializeField, Tooltip("Entity의 표시될 이름")]
-    protected string displayName;
-
-    /// <summary>
-    /// Entity의 표시될 이름
-    /// </summary>
-    public string DisplayName
+    public abstract class Entity : MonoBehaviour
     {
-        get
-        {
-            if (string.IsNullOrEmpty(displayName))
-            {
-                return entityData.Name;
-            }
-            return displayName;
-        }
 
-        set
+        #region entityData
+        /// <summary>
+        /// Entity의 공통 로직을 담고 있는 ScriptableObject
+        /// </summary>
+        [SerializeField, Tooltip("Entity의 공통 로직을 담고 있는 ScriptableObject")]
+        protected EntityData entityData;
+
+        /// <summary>
+        /// Entity의 공통 로직을 담고 있는 ScriptableObject
+        /// </summary>
+        public EntityData EntityData => entityData;
+        #endregion
+
+        #region name
+        /// <summary>
+        /// Entity의 표시될 이름
+        /// </summary>
+        [SerializeField, Tooltip("Entity의 표시될 이름")]
+        protected string displayName;
+
+        /// <summary>
+        /// Entity의 표시될 이름
+        /// </summary>
+        public string DisplayName
         {
-            if (string.IsNullOrEmpty(value))
+            get
             {
-                displayName = null;
-                return;
+                if (string.IsNullOrEmpty(displayName))
+                {
+                    return entityData.Name;
+                }
+                return displayName;
             }
-            displayName = value;
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    displayName = null;
+                    return;
+                }
+                displayName = value;
+            }
         }
+        #endregion
+
+        #region description
+        /// <summary>
+        /// Entity의 설명
+        /// </summary>
+        [SerializeField, TextArea(3, 5), Tooltip("Entity의 표시될 설명")] protected string displayDescription;
+
+        /// <summary>
+        /// Entity의 설명
+        /// </summary>
+        public string DisplayDescription
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(displayDescription))
+                {
+                    return entityData.Description;
+                }
+                return displayDescription;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    displayDescription = null;
+                    return;
+                }
+                displayDescription = value;
+            }
+        }
+        #endregion
+
     }
-    #endregion
-
-    #region description
-    /// <summary>
-    /// Entity의 설명
-    /// </summary>
-    [SerializeField, TextArea(3, 5), Tooltip("Entity의 표시될 설명")] protected string displayDescription;
-
-    /// <summary>
-    /// Entity의 설명
-    /// </summary>
-    public string DisplayDescription
-    {
-        get
-        {
-            if (string.IsNullOrEmpty(displayDescription))
-            {
-                return entityData.Description;
-            }
-            return displayDescription;
-        }
-
-        set
-        {
-            if (string.IsNullOrEmpty(value))
-            {
-                displayDescription = null;
-                return;
-            }
-            displayDescription = value;
-        }
-    }
-    #endregion
-
 }
+
