4da38ac679dc22ed153c6b466f2e18e78a93ce5e
9um4 <51898564+9um4@users.noreply.github.com>
2025-02-01 23:48:25 +0900

    Refactor: replace ItemID enum with ID enum for item identification

---
+++ Assets/Scripts/ItemData/ID.cs
@@ -0,0 +1,15 @@
+/// <summary>
+/// Item의 종류를 구별하는 구별자. ID.
+/// </summary>
+
+namespace ItemSystem
+{
+    /// <summary>
+    /// Item의 종류를 구별하는 식별자
+    /// </summary>
+    public enum ID
+    {
+        AIR,
+
+    }
+}
--- Assets/Scripts/ItemData/ItemID.cs
+++
@@ -1,7 +0,0 @@
-/// <summary>
-/// Item의 종류를 구별하는 구별자. ID.
-/// </summary>
-public enum ItemID {
-    AIR,
-
-}
